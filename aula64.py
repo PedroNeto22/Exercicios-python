f531c7f243d12930bcadea6bb5cfe455d543cc61
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2023-08-27 00:07:32 -0300

    Aula64

---
+++ aula64.py
@@ -0,0 +1,10 @@
+name = 'Pedro Neto'
+tamanho_nome = len(name)
+count = 0
+new_name = ''
+while count < tamanho_nome:
+    new_name += f"*{name[count]}"
+    count+=1
+new_name += '*'
+print(new_name)
+
