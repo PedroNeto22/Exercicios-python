d15b1227bde7e0b0afd14b04554a4af0db7ed08c
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2023-09-02 17:29:05 -0300

    aula98

--- aula98.py
+++ aula98.py
@@ -1,26 +1,48 @@
 #746.824.890-70
+# Primeira forma que eu fiz
+# cpf = input('Digite um cpf: ')
+
+# if ('.' not in cpf) and ('-' not in cpf):
+#     cpf_formatado = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
+
+# cpf_separado = cpf_formatado.split('-')
+
+# cpf_primeiros_numeros = cpf_separado[0].split('.')
+
+# multiplicador = 10
+# soma = 0
+
+# for i in range(0,(len(cpf_primeiros_numeros))):
+#     for j in range(0,(len(cpf_primeiros_numeros))):
+#         soma += int(cpf_primeiros_numeros[i][j]) * multiplicador
+#         multiplicador-= 1
+        
+# resultado = (soma*10)%11
+
+# print(resultado)
+
+# resultado = 0 if resultado > 9 else resultado
+
+# print(resultado)
+
+
+#746.824.890-70
+# Segunda forma que eu fiz
 
 cpf = input('Digite um cpf: ')
 
-cpf_separado = cpf.split('-')
-
-cpf_primeiros_numeros = cpf_separado[0].split('.')
+if ('.' in cpf) and ('-' in cpf):
+    cpf_cru = f'{cpf[:3]}{cpf[4:7]}{cpf[8:11]}{cpf[12:]}'
+else:
+    cpf_cru = cpf
 
 multiplicador = 10
 soma = 0
 
-for i in range(0,(len(cpf_primeiros_numeros))):
-    for j in range(0,(len(cpf_primeiros_numeros))):
-        soma += int(cpf_primeiros_numeros[i][j]) * multiplicador
-        multiplicador-= 1
-        
+for i in range(0,len(cpf_cru)-2):
+    soma += int(cpf_cru[i])*multiplicador
+    multiplicador -= 1
+
 resultado = (soma*10)%11
 
 print(resultado)
-
-resultado = 0 if resultado > 9 else resultado
-
-print(resultado)
-
-
-
